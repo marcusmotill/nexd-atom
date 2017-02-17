bba6d6d8445360692b843e42a45b1431d64f213c
Marcus Motill <marcus.motill@nexd.io>
2017-02-16 23:55:39 -0600

    init commit

---
+++ spec/nexd-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NexdAtomView from '../lib/nexd-atom-view';
+
+describe('NexdAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
